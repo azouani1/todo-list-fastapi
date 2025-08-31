6445bc66d805dc69b71eea402d9707d456b2ef09
azouani1 <aya.zouani@student.unamur.be>
2025-08-31 22:45:42 +0200

    Mon projet FastAPI todo-list

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import auth, tasks
 from app.middleware import log_middleware  
 from app.database import create_db_and_tables 
 
 app = FastAPI()
+
+# Sert les fichiers statiques (CSS, images, etc.)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 #  Initialise la base de données 
 create_db_and_tables()
